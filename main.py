a45113c9630bf4e0c1d012f6ec391c30031a52aa
Kate-Gornostaeva <itsmekate153@gmail.com>
2024-12-10 20:55:00 +1000

    final_final
    теперь точно конечный вариант

--- main.py
+++ main.py
@@ -17,7 +17,6 @@
 
 import random
 
-
 class Hero:
     def __init__(self, name):
         self.name = name
@@ -30,7 +29,7 @@ class Hero:
         other.health -= damage
         print(f"{self.name} атакует {other.name} и наносит {damage} урона!")
 
-#Немного отсебятины, которой нет в задании. Добавила восстановление здоровья героев. Так интереснее. Эту часть помогал делать Котик
+    # Немного отсебятины, которой нет в задании. Добавила восстановление здоровья героев. Так интереснее. Эту часть помогал делать Котик
     def use_special_ability(self):
         if not self.special_ability_used:
             heal_amount = random.randint(5, 15)
@@ -46,43 +45,45 @@ class Hero:
     def __str__(self):
         return f"{self.name}: {self.health} ед.здоровья"
 
+class Game:
+    def __init__(self):
+        self.player = None
+        self.computer = None
 
-def player_turn(player, enemy):
-    action = input("Выберите действие: 1 - Атаковать, 2 - Использовать лечение: ")
-    if action == "1":
-        player.attack(enemy)
-    elif action == "2":
-        player.use_special_ability()
-    else:
-        print("Неверный ввод. Попробуйте еще раз.")
+    def start(self):
+        player_name = input("Введите имя вашего героя: ")
+        print('Ваш герой может использовать лечение и восстановить часть своего здоровья. \nНо воспользоваться этой возможностью можно только один раз!')
+        self.player = Hero(player_name)
+        self.computer = Hero("Компьютер")
 
+        while self.player.is_alive() and self.computer.is_alive():
+            print("\nСтатусы:")
+            print(self.player)
+            print(self.computer)
 
-def computer_turn(computer, player):
-    if random.choice([True, False]) and not computer.special_ability_used:
-        computer.use_special_ability()
-    else:
-        computer.attack(player)
+            self.player_turn()
+            if self.computer.is_alive():
+                self.computer_turn()
 
+        if self.player.is_alive():
+            print(f"{self.player.name} победил!")
+        else:
+            print(f"{self.computer.name} победил!")
 
-def running_game():
-    player_name = input("Введите имя вашего героя: ")
-    print('Ваш герой может использовать лечение и восстановить часть своего здоровья. \nНо воспользоваться этой возможностью можно только один раз!')
-    player = Hero(player_name)
-    computer = Hero("Компьютер")
+    def player_turn(self):
+        action = input("Выберите действие: 1 - Атаковать, 2 - Использовать лечение: ")
+        if action == "1":
+            self.player.attack(self.computer)
+        elif action == "2":
+            self.player.use_special_ability()
+        else:
+            print("Неверный ввод. Попробуйте еще раз.")
 
-    while player.is_alive() and computer.is_alive():
-        print("\nСтатусы:")
-        print(player)
-        print(computer)
+    def computer_turn(self):
+        if random.choice([True, False]) and not self.computer.special_ability_used:
+            self.computer.use_special_ability()
+        else:
+            self.computer.attack(self.player)
 
-        player_turn(player, computer)
-        if computer.is_alive():
-            computer_turn(computer, player)
-
-    if player.is_alive():
-        print(f"{player.name} победил!")
-    else:
-        print(f"{computer.name} победил!")
-
-
-running_game()
+game = Game()
+game.start()
